9e18d4e4210d1bbda821f95d0876ca216fe536c0
renevanderark <r.van.der.ark@gmail.com>
2013-09-06 07:44:29 +0200

    small modifications

--- demo/jquery.jp2-viewer.js
+++ demo/jquery.jp2-viewer.js
@@ -1,9 +1,24 @@
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+	return window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+
 (function ( $ ) {
 	$.fn.jp2Viewer = function(filename, opts) {
 		var canvas = this.get(0);
 		var ctx = canvas.getContext('2d');
+		var bufcan = $("<canvas>").get(0);
+		var buffer = bufcan.getContext('2d');
+/*		$("body").append(bufcan);*/
+
 		var dataType = opts.dataType || "json";
-		var primary = opts.primary || "http://" + window.location.hostname;
+		var primary = opts.primary || "http://" + window.location.hostname + "/cgi-bin/jp2";
 		var workers = opts.workers || [{address: primary, cores: 1}]; 
 		var initScale = opts.initScale || "full-width";
 
@@ -18,43 +33,52 @@
 		var yPos = 0;
 		var curX;
 		var curY;
+		var oldX;
+		var oldY;
 		var dragging = false;
-		var tiles2go = 1;
+		var loadTrigger = false;
+		var showTrigger = false;
+		var incompleteTiles = [];
+
+		bufcan.width = canvas.width;
+		bufcan.height = canvas.height;
+		buffer.fillStyle = opts.backgroundColor || "#aaa";
+
 
 		this.on("mousedown", function(e) {
 			dragging = true;
 			curX = e.clientX;
 			curY = e.clientY;
+			oldX = xPos;
+			oldY = yPos;
 		});
 
 		this.on("mousemove", function(e) {
 			if(dragging) {
 				var movX = curX - e.clientX;
 				var movY = curY - e.clientY;
-				var oldX = xPos;
-				var oldY = yPos;
 				xPos -= movX;
 				yPos -= movY;
 				ensureBounds();
-				if(oldX != xPos || oldY != yPos) {
-					showImage();
-				}
+				showImage();
+				if(oldX != xPos || oldY != yPos) { loadTrigger = true; }
 				curX = e.clientX;
 				curY = e.clientY;
 			}
 		});
 
 		this.on("mouseup mouseout", function(e) {
+			if(oldX != xPos || oldY != yPos) { loadTrigger = true; }
 			dragging = false;
 		});
 
 		this.on("mousewheel", function(e, delta, deltaX, deltaY) {
 			if(delta > 0) {
 				initScale = scale * 1.1;
-				initialize(jp2Header);
+				initialize();
 			} else if(delta < 0) {
 				initScale = scale * 0.9;
-				initialize(jp2Header);
+				initialize();
 			}
 		});
 
@@ -102,22 +126,20 @@
 		function drawTile(tile) {
 			if(tile.dims.r != reduction) { return; }
 			if(tile.img.complete) {
-				tiles2go--;
-				ctx.drawImage(
+				buffer.drawImage(
 					tile.img, tile.dims.x, tile.dims.y, 
 					Math.ceil(tile.img.width * tile.dims.s), 
 					Math.ceil(tile.img.height * tile.dims.s));
 			} else {
-				ctx.clearRect(tile.dims.x, tile.dims.y, jp2Header.tdx * tile.dims.s, jp2Header.tdy * tile.dims.s);
-				setTimeout(function() { drawTile(tile); }, 1);
+				buffer.fillRect(tile.dims.x, tile.dims.y, jp2Header.tdx * tile.dims.s, jp2Header.tdy * tile.dims.s);
+				incompleteTiles.push(tile);
 			}
-			if(tiles2go == 0) { tiles2go = 1; clearSurroundings(); showImage();}
 		}
 
 		function clearSurroundings() {
 			if(jp2Header.x1 * scale <= canvas.width) { 
 				ctx.clearRect(0,0, xPos, canvas.height);
-				ctx.clearRect(canvas.width - xPos - 1, 0, xPos, canvas.height);
+				ctx.clearRect(canvas.width - xPos - 1, 0, xPos + 1, canvas.height);
 			}
 
 			if(jp2Header.y1 * scale <= canvas.height) { 
@@ -125,7 +147,7 @@
 			}
 		}
 
-		function showImage() {
+		function loadImage(_hidden) {
 			var tileS = scale / reduce(1.0, reduction);
 			var tilesX = Math.ceil(canvas.width / (jp2Header.tdx * scale)) + 1;
 			var tilesY = Math.ceil(canvas.height / (jp2Header.tdy * scale)) + 1;
@@ -138,27 +160,28 @@
 			if(tileX + tilesX > jp2Header.tw)  { tilesX = jp2Header.tw - tileX; }
 			if(tileY + tilesY > jp2Header.th)  { tilesY = jp2Header.th - tileY; }
 
-			tiles2go = tilesX * tilesY;
 			for(var x = tileX; x < tileX + tilesX; x++) {
 				for(var y = tileY; y < tileY + tilesY; y++) {
 					var tileIndex = x + (y * jp2Header.tw);
-					tiles2go++;
 
 					if(tiles["redux-" + reduction][tileIndex]) {
 						tiles["redux-" + reduction][tileIndex].dims = { 
 							x: xPos + Math.floor(x * (jp2Header.tdx * scale)), 
 							y: yPos + Math.floor(y * (jp2Header.tdy * scale)),
+							tx: x, 
+							ty: y,
 							s: tileS,
 							r: reduction
 						};
-
-						drawTile(tiles["redux-" + reduction][tileIndex]); 
+						drawTile(tiles["redux-" + reduction][tileIndex]);
 					} else {
 						var tile = { 
 							img: new Image(),
 							dims:  { 
 								x: xPos + Math.floor(x * (jp2Header.tdx * scale)), 
 								y: yPos + Math.floor(y * (jp2Header.tdy * scale)),
+								tx: x, 
+								ty: y,
 								s: tileS,
 								r: reduction
 							}
@@ -169,20 +192,13 @@
 							t: tileIndex,
 							r: reduction
 						});
-						(function(t) {
-							$(tile.img).on("error", function() {
-								console.log("TODO, retry on erronaous tile: ", t);
-							});
-						})(tile);
-
 						drawTile(tile);
 					}
 
 					if(++currentWorker == workers.length) { currentWorker = 0; }
 				}
 			}
-			clearSurroundings();
-
+			showImage();
 		}
 
 		function initialize(__init) {
@@ -191,9 +207,57 @@
 			setReduction();
 			if(__init) { initializeTiles(); }
 			ensureBounds();
+			loadImage();
+		}
+
+		function showImage() {
+			ctx.drawImage(bufcan, 0, 0);
 			clearSurroundings();
+		}
+
+		function drawIncompleteTiles() {
+			var i = incompleteTiles.length;
+			if(i == 0) { return; }
+			while(--i > -1) {
+				if(incompleteTiles[i].img.complete) {
+					incompleteTiles[i].dims.x = xPos + Math.floor(incompleteTiles[i].dims.tx * (jp2Header.tdx * scale));
+					incompleteTiles[i].dims.y = yPos + Math.floor(incompleteTiles[i].dims.ty * (jp2Header.tdy * scale)),
+					incompleteTiles[i].dims.s = scale / reduce(1.0, reduction);
+
+					drawTile(incompleteTiles[i]);
+					incompleteTiles.splice(i, 1);
+				}
+			}
 			showImage();
 		}
+
+		function preloadHiddenTiles() {
+			for(var x = 0; x < jp2Header.tw; x++) {
+				for(var y = 0; y < jp2Header.th; y++) {
+					var tileIndex = x + (y * jp2Header.tw);
+					if(!tiles["redux-" + reduction][tileIndex]) {
+						tiles["redux-" + reduction][tileIndex] = { img: new Image() };
+						tiles["redux-" + reduction][tileIndex].img.src = workers[currentWorker].address + "?" + $.param({
+							f: filename,
+							t: tileIndex,
+							r: reduction
+						});
+					}
+				}
+			}
+		}
+
+		function monitorTiles() {
+			if(!jp2Header) { return; }
+			requestAnimFrame(monitorTiles);
+			if(loadTrigger) { loadImage(); loadTrigger = false; }
+			drawIncompleteTiles();
+			if(incompleteTiles.length == 0) {
+				preloadHiddenTiles();
+			}
+		}
+
+		monitorTiles();
 
 		$.ajax(primary, {
 			data: { f: filename },
