ec3aaf3b7ffdd601cc4873d3cabcd652c3a0e3c8
renevanderark <r.van.der.ark@gmail.com>
2013-10-10 09:48:01 +0200

    stress test

--- compile_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc -I/usr/local/include/openjpeg-2.0/ lib/opj_url_stream.c lib/opj_memcached_stream.c lib/opj_res.c lib/opj2png.c  test_memcached.c -fPIC -lm -lmemcached -lpng -lopenjp2 -lcurl -Wall -Wextra -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition
---
+++ test/parallel_stress.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import threading
+import urllib2
+import time
+import sys
+import os
+
+class Task(urllib2.HTTPHandler):
+	def setup(self, url, saveOutput):
+		self.before = int(round(time.time() * 1000))
+		self.url = url
+		self.saveOutput = saveOutput
+
+	def http_response(self, req, response):
+		self.timed = int(round(time.time() * 1000)) - self.before
+		if(self.saveOutput != False):
+			data = response.read()
+			f = open("./out/" + self.saveOutput, "w")
+			f.write(data)
+			f.close()
+
+	def report(self):
+		print str(self.url) + ";" + str(self.timed)
+
+
+num_tiles = 130
+num_pages = 34
+max_concurrent = 50
+page = 0
+openThreads = []
+executedTasks = []
+saveOutput = False
+if(len(sys.argv) > 1 and sys.argv[1] == "--save-response"):
+	saveOutput = True
+	if(os.path.isdir("./out") == False):
+		os.mkdir("./out")
+	print "Saving responses to dir ./out"
+
+while page < num_pages:
+	image_url = "http://resolver.kb.nl/resolve?urn=ddd:010691742:mpeg21:p" + str(page + 1).zfill(3) + ":image"
+	tile = 0
+	while tile < num_tiles:
+		viewer_url = "http://localhost/cgi-bin/jp2?t=" + str(tile) + "&u=" + urllib2.quote(image_url) + "&r=1"
+		task = Task()
+		outfile = False
+		if(saveOutput):
+			outfile = str(page) + "-" + str(tile) + ".png"
+		task.setup(viewer_url, outfile)
+		opener = urllib2.build_opener(task)
+		if(len(openThreads) >= max_concurrent):
+			t = openThreads.pop(0)
+			t.join()
+			print "Joined: " + t.getName() + " / open: " + str(len(openThreads)) + " of total: " + str(num_tiles * num_pages)
+		t = threading.Thread(target=opener.open, args=(viewer_url,))
+		openThreads.append(t)
+		executedTasks.append(task)
+		t.start()
+		tile += 1
+	page += 1
+
+for i in range(len(openThreads)):
+	t = openThreads.pop(0)
+	t.join()
+	print "Joined: " + t.getName() + " / open: " + str(len(openThreads)) + " of total: " + str(num_tiles * num_pages)
+
+s = 0
+for i in range(len(executedTasks)):
+	s += executedTasks[i].timed
+
+print "Average response time: " + str(s / len(executedTasks)) + "ms"
