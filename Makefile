8919b51f2f0e74fe0a4521c341e6e9ed0ae1365b
renevanderark <r.van.der.ark@gmail.com>
2013-10-11 11:38:35 +0200

    added cache cleaner job

--- Makefile
+++ Makefile
@@ -2,12 +2,17 @@ all: clean compile
 
 clean:
 	rm -f jp2.cgi
+	rm -f jp2-cache-clean
 
 compile:
 	gcc -I/usr/local/include/openjpeg-2.0/ lib/url2cache.c lib/opj_res.c lib/opj2png.c lib/urldecode.c jp2read.c -o jp2.cgi -fPIC -lm -lpng -lopenjp2 -lcurl -std=c99 -Wall -Wextra -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition -D_XOPEN_SOURCE=700
+	gcc jp2-cache-clean.c -o jp2-cache-clean
 
 install:
 	cp jp2.cgi /usr/lib/cgi-bin/jp2
+	cp jp2-cache-clean /usr/local/bin/jp2-cache-clean
+	chmod 755 /usr/local/bin/jp2-cache-clean
 
 uninstall:
 	rm -f /usr/lib/cgi-bin/jp2
+	rm -f /usr/local/bin/jp2-cache-clean
---
+++ jp2-cache-clean.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <getopt.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+
+struct file_node {
+	time_t lifetime;
+	size_t size;
+	char fullpath[255];
+	struct file_node *next;
+};
+
+static int verbose = 0;
+
+static void print_file_node(struct file_node *f) {
+	if(verbose) fprintf(stderr, "[INFO] %s:\tlifetime=%zus\tsize=%zu(MB)\n\n", f->fullpath, f->lifetime, f->size / 1024 / 1024);
+}
+
+static void free_list(struct file_node *list) {
+	if(list == NULL) { return; }
+	if(list->next != NULL) { free_list(list->next); }
+
+	free(list);
+}
+
+static struct file_node *insert(char *fullpath, time_t lifetime, size_t filesize, struct file_node *list) {
+	struct file_node *new_node = malloc(sizeof(struct file_node));
+
+	memcpy((char*)&new_node->fullpath, fullpath, 255);
+	new_node->lifetime = lifetime;
+	new_node->size = filesize;
+	new_node->next = NULL;
+
+	if(list == NULL) { return new_node; }
+	else if(list->lifetime < new_node->lifetime) {
+		new_node->next = list;
+		return new_node;
+	}
+
+	struct file_node *cur_node = list;
+
+	while(cur_node->next != NULL) {
+		if(cur_node->next->lifetime < new_node->lifetime) {
+			new_node->next = cur_node->next;
+			cur_node->next = new_node;
+			return list;
+		}
+		cur_node = cur_node->next;
+	}
+
+	cur_node->next = new_node;
+	return list;
+}
+
+static int apply_cache_rules(char *cachedir, time_t lifetime, size_t max_size) {
+	DIR *dirp;
+	struct dirent *dp;
+	time_t now;
+	size_t dir_usage = 0;
+	size_t dir_usage_mb = 0;
+
+	struct file_node *list = NULL;
+
+	if((dirp = opendir(cachedir)) == NULL) {
+		if(verbose) fprintf(stderr, "[INFO] Could not open cache directory: %s\n", cachedir);
+		return 1;
+	}
+
+	time (&now);
+	while((dp = readdir(dirp)) != NULL) {
+		struct stat statbuf;
+		char fullpath[255];
+
+		sprintf(fullpath, "%s/%s", cachedir, dp->d_name);
+
+		if(strcmp(".", dp->d_name) == 0 || strcmp("..", dp->d_name) == 0) { continue; }
+
+		if(stat(fullpath, &statbuf) == 0) {
+			time_t file_lifetime = now - statbuf.st_atime;
+			if(lifetime < file_lifetime) {
+				if(verbose) fprintf(stderr, "[INFO] Removing expired file: %s\n", fullpath);
+				if(unlink(fullpath) < 0) { 
+					fprintf(stderr, "[ERROR] failed to remove file: %s\n", fullpath); 
+					free_list(list);
+					return 1;
+				}
+				continue;
+			}
+
+			dir_usage += statbuf.st_size;
+			list = insert(fullpath, file_lifetime, statbuf.st_size, list);
+		}
+	}
+
+	if(dir_usage / 1024 / 1024 > max_size) {
+		if(verbose) fprintf(stderr, "[INFO] Directory in excess of max usage: %zuMB / %zuMB\n", dir_usage / 1024 / 1024, max_size);
+
+		struct file_node *cur_node = list;
+		while(cur_node != NULL) {
+			if(verbose) fprintf(stderr, "[INFO] Removing oldest remaining file: %s\n", cur_node->fullpath);
+
+			if(unlink(cur_node->fullpath) < 0) { 
+				fprintf(stderr, "[ERROR] failed to remove file: %s\n", cur_node->fullpath); 
+				free_list(list);
+				return 1;
+			}
+			dir_usage -= cur_node->size;
+			if(dir_usage / 1024 / 1024 < max_size) { break; }
+			cur_node = cur_node->next;
+		}
+	}
+
+	free_list(list);
+	closedir(dirp);
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	static struct option opts[] = {
+		{"cache-dir", required_argument, NULL, 'd'},
+		{"lifetime", required_argument, NULL, 't'},
+		{"max-size", required_argument, NULL, 'm'},
+		{"verbose", no_argument, NULL, 'v'},
+		{NULL, 0, NULL, 0}
+	};
+	char ch;
+	char *cachedir = NULL;
+	time_t lifetime = 3600;
+	size_t max_size = 1024; 
+	while((ch = getopt_long(argc, argv, "d:t:", opts, NULL)) != -1) {
+		switch(ch) {
+			case 'v':
+				verbose = 1;
+				break;
+			case 'd':
+				cachedir = optarg;
+				break;
+			case 't':
+				lifetime = (time_t) strtol(optarg, NULL, 0);
+				break;
+			case 'm':
+				max_size = (size_t) strtol(optarg, NULL, 0);
+		}
+	}
+	if(cachedir == NULL) { 
+		printf("Usage: %s --cache-dir /path/to/cache\n", argv[0]); 
+		fprintf(stderr, "[ERROR] No cache dir specified\n", argv[0]); 
+		return 1; 
+	}
+
+	
+	return apply_cache_rules(cachedir, lifetime, max_size);
+}
