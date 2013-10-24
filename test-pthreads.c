44f07db39496e384fdf09b2a5ceb02596d29a74e
renevanderark <r.van.der.ark@gmail.com>
2013-10-24 15:14:21 +0200

    pthreads

--- test-pthreads.c
+++ test-pthreads.c
@@ -24,11 +24,8 @@ typedef struct thr_res {
 		unsigned num_comps;
 		OPJ_UINT32 tw;
 		OPJ_UINT32 th;
-//		JSAMPLE **rgb;
 		chunked_scanline_t *scanlines;
 } thr_res_t;
-
-
 
 typedef struct thr_arg {
 		opj_dparameters_t *params;
@@ -45,15 +42,10 @@ static void write_scanlines(opj_image_t *image, unsigned tile_index, thr_res_t *
 	unsigned tileX = tile_index - (tres->tilesX * tileY);
 	unsigned xPos = tileX * tres->tw;
 	unsigned yPos = tileY * tres->th;
-
-/*	printf("write scanlines from: %dpx / %dpx\n", xPos, yPos);
-	printf("to: %dpx %dpx\n", xPos + image->comps[0].w, yPos+ image->comps[0].h);*/
 	unsigned x, y, i = 0;
 	
 	for(y = yPos; y < yPos + image->comps[0].h; y++) {
 		for(x = xPos; x < xPos + image->comps[0].w; x++, i++) {
-//			printf("(%d/%d)\n", x, y);
-
 			if(tres->num_comps < 3) {
 				tres->scanlines[y].rgb[x] = image->comps[0].data[i];
 			} else {
@@ -61,11 +53,9 @@ static void write_scanlines(opj_image_t *image, unsigned tile_index, thr_res_t *
 				tres->scanlines[y].rgb[x*tres->num_comps+1] = image->comps[1].data[i];
 				tres->scanlines[y].rgb[x*tres->num_comps+2] = image->comps[2].data[i];
 			}
-//			printf("\t--> (%d / %d / %d)\n", tres->rgb[y][x*tres->num_comps], tres->rgb[y][x*tres->num_comps + 1], tres->rgb[y][x*tres->num_comps + 2]);
 		}
 		tres->scanlines[y].chunks_done++;
 	}
-//	printf("FINE\n");
 }
 
 static void *processTile(void *args) {
@@ -73,21 +63,10 @@ static void *processTile(void *args) {
 		unsigned tile_index_end = ((thr_args_t*)args)->tile_index_end;
 		unsigned tile_index;
 		for(tile_index = tile_index_start; tile_index < tile_index_end; tile_index++) {
-
-/*				char *outfile = malloc(sizeof(char) * 21);
-				sprintf(outfile, "output/tile-%03d.jpg", tile_index);
-				FILE *fp = fopen(outfile, "wb");*/
-
 				opj_dparameters_t *params = ((thr_args_t*)args)->params;
 				struct opj_res res = opj_init(((thr_args_t*)args)->cachefile, params);
 				opj_get_decoded_tile(res.l_codec, res.l_stream, res.image, tile_index);
-
 				write_scanlines(res.image, tile_index, ((thr_args_t*)args)->tres);
-
-/*				writeJPEG(&res, 0, 0, 0, 0, 3, fp);
-				fclose(fp);
-				free(outfile);*/
-
 				opj_cleanup(&res);
 		}
 		((thr_args_t*)args)->tres->done++;
@@ -142,40 +121,62 @@ int main(int argc, char **argv) {
 
 		opj_destroy_cstr_info(&info);
 		opj_cleanup(&resources);
-		printf("TILES: %d / %d\n",tres.tilesX, tres.tilesY);
-		printf("TILE DIMS: %dpx / %dpx\n", tres.tw, tres.th);
-		printf("IMAGE DIMS: %dpx / %dpx\n", x1, y1);
 
 		for(n = 0; n < n_thr; n++) {
 				args[n].tres = &tres;
 				args[n].cachefile = cachefile;
 				args[n].tile_index_start = (n * tres.tilesX * tres.tilesY) / n_thr;
 				args[n].tile_index_end = ((n+1) * tres.tilesX * tres.tilesY) / n_thr;
-//				printf("%d-%d\n", args[n].tile_index_start, args[n].tile_index_end);
 				args[n].params = &parameters;
 				pthread_create(&t[n], NULL, processTile, &args[n]);
-//				processTile(&args[n]);
 		}
 
 		y = 0;
-		while(tres.done < n_thr) { 
-			if(tres.scanlines[y].chunks_done == tres.scanlines[y].n_chunks) {
-				printf("line: %d is done\n", y++);
-			}
+		while(tres.done < n_thr) {  }
 
+		FILE *fp = fopen("outfile.jpg", "wb");
+		struct jpeg_compress_struct cinfo;
+		struct jpeg_error_mgr		jerr;
+
+		cinfo.err = jpeg_std_error(&jerr);
+		jpeg_create_compress(&cinfo);
+		jpeg_stdio_dest(&cinfo, fp);
+
+		cinfo.image_width = x1;
+		cinfo.image_height = y1;
+		cinfo.in_color_space = JCS_RGB;
+		if(tres.num_comps < 3)	{
+			cinfo.in_color_space = JCS_GRAYSCALE;
+			cinfo.input_components = 1;
+		} else {
+			cinfo.in_color_space	= JCS_RGB;
+			cinfo.input_components = 3;
 		}
 
+		jpeg_set_defaults(&cinfo);
+		jpeg_set_quality (&cinfo, 100, 1);
+		jpeg_start_compress(&cinfo, 1);
+		JSAMPROW row_pointer[1];
+		JSAMPLE rgb[x1 * tres.num_comps];
+		unsigned i;
 		for(y = 0; y < y1; y++) {
-//			printf("(\n");
 			for(x = 0; x < x1; x++) {
-//				printf("\t(%d, %d, %d)\n", tres.scanlines[y].rgb[x],tres.scanlines[y].rgb[x+1],tres.scanlines[y].rgb[x+2] );
+				for(i = 0; i < tres.num_comps; i++) {
+					rgb[x*tres.num_comps+i] = tres.scanlines[y].rgb[x*tres.num_comps+i];
+				}
 			}
-//			printf(")\n");
+			row_pointer[0] = (JSAMPROW) &rgb;
+			(void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+
 			free(tres.scanlines[y].rgb);
 		}
+		jpeg_finish_compress(&cinfo);
+		jpeg_destroy_compress(&cinfo);
+		
+
 		free(tres.scanlines);
 		free(cachefile);
-
+		fclose(fp);
 		pthread_exit(NULL);
 		return 0;
 }
